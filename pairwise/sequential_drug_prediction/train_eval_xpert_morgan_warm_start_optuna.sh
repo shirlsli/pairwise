16c9c947564f9893d4e24b7ecb8fb6c521caeaac
Shirley Li <ssl4003@cayuga-login1.localdomain>
2026-05-06 17:54:02 -0400

    chore: push more scripts

---
+++ pairwise/sequential_drug_prediction/train_eval_xpert_morgan_warm_start_optuna.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH --partition=scu-gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=train_eval_xpert_morgan_warm_start_optuna_fold_%a
+#SBATCH --mem-per-gpu=60GB
+#SBATCH --gres=gpu:1
+#SBATCH --array=0-4
+
+source ~/.bashrc
+conda activate sequential_prediction_311
+
+python -u pipeline.py \
+    --config_path /athena/angsd/scratch/ssl4003/sequential_drug_combination/pairwise/pairwise/sequential_drug_prediction/configs/config_optuna_best.json \
+    --cell_line_consensus \
+    --drug_format morgan \
+    --model_output_dir /athena/angsd/scratch/ssl4003/sequential_drug_combination/pairwise/pairwise/sequential_drug_prediction/best_models_optuna \
+    --processed_data_dir /athena/angsd/scratch/ssl4003/sequential_drug_combination/pairwise/data/perturbation_data/processed_lincs_6_24_hrs_9-11_uM_cell_baseline_consensus.pkl \
+    --warm_start \
+    --fold_output_dir /athena/angsd/scratch/ssl4003/sequential_drug_combination/pairwise/data/perturbation_data/folds_warm_start \
+    --ppi_gene_vector_path /athena/angsd/scratch/ssl4003/sequential_drug_combination/pairwise/data/perturbation_data/PPI_gene_vector_128d.npy \
+    --fold_idx $SLURM_ARRAY_TASK_ID
